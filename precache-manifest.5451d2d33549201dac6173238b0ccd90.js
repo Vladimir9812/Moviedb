fecce1cf6d163c26d8f550ebf5738c27e2e6ca60
Vladimir9812 <fomin.v97@yandex.ru>
2020-03-16 15:06:56 +0300

    movie

---
+++ precache-manifest.5451d2d33549201dac6173238b0ccd90.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d76d0bea5a12f678c16",
+    "url": "/static/css/main.c808ea9b.chunk.css"
+  },
+  {
+    "revision": "2d76d0bea5a12f678c16",
+    "url": "/static/js/main.7202ed16.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a9a0e51c499574da24b9",
+    "url": "/static/css/2.7b5e8be2.chunk.css"
+  },
+  {
+    "revision": "a9a0e51c499574da24b9",
+    "url": "/static/js/2.7e2e78a0.chunk.js"
+  },
+  {
+    "revision": "5f280ca1fb86d02d68ced77276669bd0",
+    "url": "/static/media/logo.5f280ca1.png"
+  },
+  {
+    "revision": "ad7a38f732051ca251311c822845ca91",
+    "url": "/static/media/user.ad7a38f7.png"
+  },
+  {
+    "revision": "c687256ec30e9fa0e6c50ca81bb9d96a",
+    "url": "/static/media/footer.c687256e.png"
+  },
+  {
+    "revision": "15f21fad2c4ff0488c67e203a792e9d6",
+    "url": "/static/media/star.15f21fad.png"
+  },
+  {
+    "revision": "ff2e8b544b2f256b1f7274d4d793605f",
+    "url": "/static/media/heart.ff2e8b54.png"
+  },
+  {
+    "revision": "228d0151eaef62eb7e994d9ab3dee6e7",
+    "url": "/static/media/play.228d0151.png"
+  },
+  {
+    "revision": "a5046e7493cf0c539f28c6491cf112f4",
+    "url": "/static/media/404.a5046e74.png"
+  },
+  {
+    "revision": "a6a0c970aef388e30b6dd0a49f05ff58",
+    "url": "/static/media/advanture.a6a0c970.jpg"
+  },
+  {
+    "revision": "b042bc11bf77c820d04bfba0d005719c",
+    "url": "/index.html"
+  }
+];
